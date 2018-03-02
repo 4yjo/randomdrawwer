aaa7bec22ea47e4333d6f20c8d77870002a4a0f1
4yjo <annameik@hotmail.de>
2018-03-02 16:40:31 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'sgThq5ZKBmscIzVN0WTr97zAG',
+  consumer_secret:      '6xf6PEYAh08GpRn8h1AQjRFfvaH0HTzT67rQCvGAUjBXqGH4fb',
+  access_token:         '965229536764981248-53bD2hqeKw5vojfVFx7g1UXl0O7kUsn',
+  access_token_secret:  'W9byxJZWphtBaKMEu8xNlojKu5CxE2s4A4HwqGxU1gLFc'
+}
