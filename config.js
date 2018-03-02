27c28fcae3c593231deaf9f8d5408c51ab7c5c77
4yjo <annameik@hotmail.de>
2018-03-02 16:44:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+// find keys and tokens of your twitter profile here: https://apps.twitter.com
+
 module.exports = {
-  consumer_key:         'sgThq5ZKBmscIzVN0WTr97zAG',
-  consumer_secret:      '6xf6PEYAh08GpRn8h1AQjRFfvaH0HTzT67rQCvGAUjBXqGH4fb',
-  access_token:         '965229536764981248-53bD2hqeKw5vojfVFx7g1UXl0O7kUsn',
-  access_token_secret:  'W9byxJZWphtBaKMEu8xNlojKu5CxE2s4A4HwqGxU1gLFc'
+  consumer_key:         'XXX',
+  consumer_secret:      'XXX',
+  access_token:         'XXX',
+  access_token_secret:  'XXX'
 }
